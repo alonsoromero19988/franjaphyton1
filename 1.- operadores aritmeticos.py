ed6ec4f4ed57c0cc5c04aa8eb4f349f4f290165f
alonsoromero19988 <103603027+alonsoromero19988@users.noreply.github.com>
2022-04-13 17:07:26 -0400

    operadores aritmeticos y variables

---
+++ 1.- operadores aritmeticos.py
@@ -0,0 +1,23 @@
+print("suma")
+print(2 + 3     )
+
+print("resta")
+print(5 - 3)
+
+print("multiplicacion")
+print(5 * 3)
+
+print("division")
+print(21 / 5)
+
+print("division entera")
+print( 21 // 5 )
+
+print("division modulo")
+print(21 % 5)
+
+print("exponencial")   
+print( 2 ** 3)
+
+# reglas de procedencia 
+print( 5 + 5 * 2)
